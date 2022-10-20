a6db8f2ba9e9ee965b05f92ef6de984c30752e70
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-20 23:50:09 +0200

    Responsive layout setup

---
+++ lib/Responsive/web_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WebView extends StatelessWidget {
+  const WebView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Text("Web View"),
+      );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/Responsive/Responsive_layout.dart';
+import 'package:instagram_clone/Responsive/mobile_view.dart';
+import 'package:instagram_clone/Responsive/web_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,11 +18,11 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Instagram Clone",
-      home: Scaffold(
-        body: Text("Hello"),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "Instagram Clone",
+        home: ResponsiveLayout(
+          mobileView: MobileView(),
+          webView: WebView(),
+        ));
   }
 }
