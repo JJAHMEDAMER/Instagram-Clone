ce8aa25002bfc83fb88da7d86d3b8eab76342846
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-22 19:57:47 +0200

    initialize app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/Responsive/Responsive_layout.dart';
 import 'package:instagram_clone/Responsive/mobile_view.dart';
 import 'package:instagram_clone/Responsive/web_view.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyD5ojhnjzF7NGgeIExTp9P1pyByvslNjqA',
+        appId: "1:480560150043:web:4f4750b81242042c0beabe",
+        messagingSenderId: "480560150043",
+        projectId: "instagram-clone-jjahmedamer",
+        storageBucket: "instagram-clone-jjahmedamer.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
@@ -17,12 +34,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Instagram Clone",
-        home: ResponsiveLayout(
-          mobileView: MobileView(),
-          webView: WebView(),
-        ));
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Instagram Clone",
+      home: ResponsiveLayout(
+        mobileView: MobileView(),
+        webView: WebView(),
+      ),
+    );
   }
 }
